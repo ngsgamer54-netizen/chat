692e58fe4fa2d315a4905f7d8c58ba1cf2919e8d
ngsgamer54-netizen <ngsgamer54@gmail.com>
2026-01-01 21:24:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,47 @@
-// Aapka Render Backend Connection
 const socket = io("https://chat-tr1m.onrender.com");
-
 let myName = "";
 
-// Chat Join karne ka function (WhatsApp Login Style)
 function joinChat() {
     const input = document.getElementById("username");
     myName = input.value.trim();
-    
-    if (myName !== "") {
-        // Login screen chhupao aur Chat UI dikhao
+    if (myName) {
         document.getElementById("login").classList.add("hidden");
         document.getElementById("chat-ui").classList.remove("hidden");
-        console.log("Joined as: " + myName);
-    } else {
-        alert("Please enter your name to start chatting!");
     }
 }
 
-// Message bhejne ka function
 function sendMsg() {
     const input = document.getElementById("msg-input");
-    const messageText = input.value.trim();
-    
-    if (messageText !== "") {
-        const msgData = { 
-            user: myName, 
-            text: messageText,
+    if (input.value.trim()) {
+        const msgData = {
+            user: myName,
+            text: input.value,
             time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
         };
-
-        // Server ko message bhejna
         socket.emit("send_message", msgData);
-        
-        // Input box khali karna
         input.value = "";
     }
 }
 
-// Enter key se message bhejne ka feature
-document.getElementById("msg-input")?.addEventListener("keypress", function(e) {
-    if (e.key === "Enter") {
-        sendMsg();
-    }
+// Enter key support
+document.getElementById("msg-input")?.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") sendMsg();
 });
 
-// Jab server se koi naya message aaye (Receive Message)
 socket.on("receive_message", (data) => {
     const box = document.getElementById("chat-box");
     const div = document.createElement("div");
-    
-    // Check karna ki message 'Mera' hai ya 'Dost' ka
     const isMe = data.user === myName;
-    
-    // WhatsApp Style Classes: 'my-message' (Right) aur 'friend-message' (Left)
+
     div.className = `message-box ${isMe ? "my-message" : "friend-message"}`;
-    
     div.innerHTML = `
         <p>
-            <b style="font-size: 0.75em; color: ${isMe ? '#075e54' : '#e91e63'}; display: block; margin-bottom: 2px;">
-                ${isMe ? "You" : data.user}
-            </b>
+            <b style="font-size: 0.75em; color: #075e54; display: block;">${isMe ? "You" : data.user}</b>
             ${data.text}
-            <span style="display: block; font-size: 0.7em; opacity: 0.5; text-align: right; margin-top: 5px;">
-                ${data.time}
-            </span>
+            <span>${data.time}</span>
         </p>
     `;
     
-    // Chat box mein message add karna
     box.appendChild(div);
-    
-    // Automatically niche scroll karna
     box.scrollTop = box.scrollHeight;
 });
