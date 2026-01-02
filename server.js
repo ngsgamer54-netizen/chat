78f98ad71274fe00873fae9876831052891e134e
ngsgamer54-netizen <ngsgamer54@gmail.com>
2026-01-02 12:19:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
 
-io.on('connection', (socket) => {
-    console.log('User Connected');
-    socket.on('send_message', (data) => {
-        io.emit('receive_message', data);
+let users = {};
+
+io.on("connection", (socket) => {
+    socket.on("user_join", (data) => {
+        users[socket.id] = { name: data.name, photo: data.photo, id: socket.id };
+        io.emit("update_users", users);
+    });
+
+    socket.on("send_message", (data) => {
+        io.emit("receive_message", data);
+    });
+
+    // --- Video Calling Signaling ---
+    socket.on("call_user", (data) => {
+        io.to(data.userToCall).emit("incoming_call", {
+            signal: data.signalData,
+            from: data.from,
+            name: data.name
+        });
+    });
+
+    socket.on("answer_call", (data) => {
+        io.to(data.to).emit("call_accepted", data.signal);
+    });
+
+    socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("update_users", users);
     });
 });
 
-server.listen(5000, () => console.log("Server running on port 5000"));
+server.listen(process.env.PORT || 3000, () => console.log("Server running..."));
